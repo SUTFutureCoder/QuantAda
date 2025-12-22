ce2de107940ef6200efa80854f7562be89c51733
Xingchen Lin <FutureCoder@aliyun.com>
2025-12-22 17:14:05 +0800

    revert sxsc

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ TUSHARE_TOKEN = 'your_tushare_token_here'
 
 # 山西证券Tushare API Token
 # 用于演示快速接入数据源
-SXSC_TUSHARE_TOKEN = 'your_tushare_token_here'
+SXSC_TUSHARE_TOKEN = 'your_sxsc_tushare_token_here'
 
